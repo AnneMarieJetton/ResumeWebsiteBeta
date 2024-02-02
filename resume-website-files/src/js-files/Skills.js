7ec226eb028518175e289e9a9fea2640b3e66321
Anne Marie Jetton <annemariejetton@gmail.com>
2024-02-02 15:58:36 -0700

    fixed skills text

--- resume-website-files/src/js-files/Skills.js
+++ resume-website-files/src/js-files/Skills.js
@@ -1,11 +1,44 @@
 import '../css-files/Skills.css';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Astronaught from '../other-files/true_astronaught_2_transparent.png';
 
 const Skills = () => {
+
+    const mainTextSkillsParentRef = useRef(null);
+    const [divWidth, setDivWidth] = useState(0);
+
+    const handleResize = () => {
+        if (mainTextSkillsParentRef.current) {
+            setDivWidth(mainTextSkillsParentRef.current.clientWidth);
+        }
+    };
+
+    useEffect(() => {
+        if (mainTextSkillsParentRef.current) {
+            setDivWidth(mainTextSkillsParentRef.current.clientWidth);
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [mainTextSkillsParentRef]);
+
+    const calculateFontSize = (minFontSize, maxFontSize) => {
+        const minContainerWidth = 750;
+      
+        // Adjust the scaleFactor based on your requirements
+        const scaleFactor = (Math.max(minContainerWidth, divWidth) / 750) * 2.5; // Using 750 as the reference width
+      
+        const fontSize = minFontSize * scaleFactor; // Assuming the initial font size is 10px
+      
+        return Math.min(maxFontSize, Math.max(minFontSize, fontSize)) + 'px';
+    };
+
     return (
-        <div className= 'MainTextSkillsParent' id="Skills">
+        <div className= 'MainTextSkillsParent' id="Skills" ref={mainTextSkillsParentRef}>
             <div className='MainTextSkillsTitle MainTextAnimation'>
                 <span>S</span>
                 <span>k</span>
@@ -15,7 +48,7 @@ const Skills = () => {
                 <span>s</span>
                 {/* Skills */}
             </div>
-            <div className='MainTextSkillsBody'>
+            <div className='MainTextSkillsBody' style={{ fontSize: calculateFontSize(4, 20) }}>
                 <div className='MainTextSkillsBodyLeft'>
                     {/* <div className='CenteredSubtitles'> */}
                         <span className='MainTextLanguagesSubtitle'>Languages</span>
@@ -38,12 +71,12 @@ const Skills = () => {
                     <span className='MainTextSkillsBodyItem' style={{ left: '33.33%', top: '10%' }}>React.JS</span>
                     <span className='MainTextSkillsBodyItem' style={{ left: '63.33%', top: '18%' }}>THREE.JS</span>
                     <span className='MainTextSkillsBodyItem' style={{ left: '23.33%', top: '26%' }}>Vue</span>
-                    <span className='MainTextSkillsBodyItem' style={{ left: '73.33%', top: '34%' }}>SQL</span>
+                    <span className='MainTextSkillsBodyItem' style={{ left: '53.33%', top: '34%' }}>SQL</span>
                     <span className='MainTextSkillsBodyItem' style={{ left: '13.33%', top: '42%' }}>Jenkins</span>
                     <span className='MainTextSkillsBodyItem' style={{ left: '63.33%', top: '50%' }}>Artifactory</span>
-                    <span className='MainTextSkillsBodyItem' style={{ left: '33.33%', top: '58%' }}>Git</span>
+                    <span className='MainTextSkillsBodyItem' style={{ left: '43.33%', top: '58%' }}>Git</span>
                     <span className='MainTextSkillsBodyItem' style={{ left: '83.33%', top: '66%' }}>JIRA</span>
-                    <span className='MainTextSkillsBodyItem' style={{ left: '53.33%', top: '74%' }}>Firebase</span>
+                    <span className='MainTextSkillsBodyItem' style={{ left: '33.33%', top: '74%' }}>Firebase</span>
                     <span className='MainTextSkillsBodyItem' style={{ left: '73.33%', top: '82%' }}>Stripe</span>
                     <span className='MainTextSkillsBodyItem' style={{ left: '13.33%', top: '90%' }}>Pygame</span>
                     <span className='MainTextSkillsBodyItem' style={{ left: '63.33%', top: '98%' }}>Postman</span>
